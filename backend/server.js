8be88e0f50135e1e40fe1f9d10e9d0868782e806
firasabidli <abidlifiras@bizerte.r-iset.tn>
2025-02-01 12:00:27 +0100

    fix port mobile frontend on 5555

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ['http://localhost:5000', 'http://localhost:51664'], // Ajouter le port exact de l'application Flutter
+    origin: ['http://localhost:5000', 'http://localhost:5555'], // Ajouter le port exact de l'application Flutter
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   })
