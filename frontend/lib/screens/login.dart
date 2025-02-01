6c086a1d4791acd9272e2e072405332566ec2c6b
firasabidli <abidlifiras@bizerte.r-iset.tn>
2025-02-01 14:11:51 +0100

    fix row code login

--- frontend/lib/screens/login.dart
+++ frontend/lib/screens/login.dart
@@ -84,96 +84,156 @@ class _LoginScreenState extends State<LoginScreen> {
     }
   }
 
-  @override
+   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: const BoxDecoration(color: Color(0xFF3C41CF)),
+        decoration: const BoxDecoration(
+          color: Color(0xFF3C41CF),
+        ),
         child: SingleChildScrollView(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Align(
-                alignment: Alignment.topCenter,
-                child: Image.asset('assets/image_login.png', height: 350),
-              ),
-              const SizedBox(height: 16),
-              const Text("Bienvenue sur", style: TextStyle(fontSize: 16, color: Colors.white)),
-              const Text(
-                "HOMELAND",
-                style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold, color: Colors.white, letterSpacing: 2),
-              ),
-              const SizedBox(height: 16),
-              _buildTextField(_emailController, "Email"),
-              const SizedBox(height: 16),
-              _buildTextField(_passwordController, "Mot de passe", obscureText: true),
-              const SizedBox(height: 24),
-              _buildLoginButton(),
-              const SizedBox(height: 16),
-              _buildTextButton("Mot de passe oublié ?", () {}),
-              const SizedBox(height: 16),
-              _buildTextButton("Créer un compte", () => Navigator.pushNamed(context, '/signup')),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildTextField(TextEditingController controller, String hintText, {bool obscureText = false}) {
-    return TextField(
-      controller: controller,
-      obscureText: obscureText,
-      decoration: InputDecoration(
-        hintText: hintText,
-        hintStyle: const TextStyle(color: Colors.black),
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(25), borderSide: BorderSide.none),
-        filled: true,
-        fillColor: Colors.white,
-        contentPadding: const EdgeInsets.symmetric(horizontal: 16),
-      ),
-    );
-  }
-
-  Widget _buildLoginButton() {
-    return Center(
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 300),
-        width: _isLoading ? 50 : 200,
-        height: 50,
-        decoration: BoxDecoration(
-          color: const Color(0xFF83FF7D),
-          borderRadius: BorderRadius.circular(25),
-          boxShadow: [
-            BoxShadow(color: Colors.black.withOpacity(0.1), blurRadius: 4, offset: const Offset(0, 3)),
-          ],
-        ),
-        child: _isLoading
-            ? const Center(child: CircularProgressIndicator(color: Colors.white, strokeWidth: 2))
-            : GestureDetector(
-                onTap: _login,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Icon(Icons.login, color: Colors.white),
-                    SizedBox(width: 8),
-                    Text("Se connecter", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white)),
-                  ],
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Image.asset(
+                    'assets/image_login.png',
+                    height: 350,
+                  ),
                 ),
-              ),
-      ),
-    );
-  }
+                const SizedBox(height: 16),
+                const Text(
+                  "Bienvenue sur",
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+                const Text(
+                  "HOMELAND",
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    letterSpacing: 2,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    hintStyle: const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25),
+                      borderSide: BorderSide.none,
+                    ),
+                    filled: true,
+                    fillColor: const Color.fromARGB(255, 255, 255, 255),
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    hintText: 'Mot de passe',
+                    hintStyle: const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25),
+                      borderSide: BorderSide.none,
+                    ),
+                    filled: true,
+                    fillColor: const Color.fromARGB(255, 255, 255, 255),
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+                  ),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 24),
+               Center(
+  child: AnimatedContainer(
+    duration: const Duration(milliseconds: 300),
+    width: _isLoading ? 50 : 200,
+    height: 50,
+    decoration: BoxDecoration(
+      color: const Color(0xFF83FF7D),
+      borderRadius: BorderRadius.circular(25),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.1),
+          blurRadius: 4,
+          offset: const Offset(0, 3),
+        ),
+      ],
+    ),
+    child: _isLoading
+        ? const Center(
+            child: CircularProgressIndicator(
+              color: Colors.white,
+              strokeWidth: 2,
+            ),
+          )
+        : GestureDetector(
+            onTap: _login,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(Icons.login, color: Colors.white),
+                const SizedBox(width: 8),
+                // Utilisez Flexible pour éviter les débordements
+                Flexible(
+                  child: const Text(
+                    "Se connecter",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
+            ),
+          ),
+  ),
+),
 
-  Widget _buildTextButton(String text, VoidCallback onTap) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Center(
-        child: Text(
-          text,
-          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+                const SizedBox(height: 16),
+                GestureDetector(
+                  onTap: () {
+                    // Action pour le mot de passe oublié
+                  },
+                  child: const Center(
+                    child: Text(
+                      "Mot de passe oublié ?",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/signup');
+                  },
+                  child: const Center(
+                    child: Text(
+                      "Créer un compte",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
